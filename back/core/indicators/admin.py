1a0f645bc7166c15d5c66173fcc2d5d3bd020ac1
lazaroah <lazaroah02@gmail.com>
2024-03-16 11:14:08 -0400

    adding indicators model to backend

--- back/core/indicators/admin.py
+++ back/core/indicators/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from indicators.models import Indicators
 
 # Register your models here.
+admin.site.register(Indicators)
--- back/core/indicators/models.py
+++ back/core/indicators/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from projects.models import Project
 
 # Create your models here.
+class Indicators(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    #Valor Actual Neto (VAN)
+    VAN = models.FloatField()
+    #Tasa Interna de Retorno (TIR)
+    TIR = models.FloatField()
+    #Tiempo de Recuperación de la Inversión (TRI)
+    TRI = models.FloatField()
+    #Costo Nivelado de la Electricidad (LEC)
+    LEC = models.FloatField()
+    #Potencial de Biometano Generado (BPM)
+    BPM = models.FloatField()
+    #Eficiencia del proceso (n)
+    n = models.IntegerField()
+
+    def __str__(self):
+        return f"Indicadores del proyecto {self.project.name}"    
