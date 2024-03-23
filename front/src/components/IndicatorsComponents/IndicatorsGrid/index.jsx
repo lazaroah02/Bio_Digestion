1b0304f9bcd878f2a5791a6016c98a5e17e66c4b
lazaroah <lazaroah02@gmail.com>
2024-03-23 15:20:08 -0400

    styling indicators card

--- front/src/components/IndicatorsComponents/IndicatorsGrid/index.jsx
+++ front/src/components/IndicatorsComponents/IndicatorsGrid/index.jsx
@@ -1,16 +1,76 @@
-import './index.css'
+import "./index.css";
 
-function IndicatorsGrid({indicators}) {
-    return ( 
-        <section className = "indicators-grid">
-            <div className = "indicator-card">{indicators?.VAN.toFixed(2)}</div>
-            <div className = "indicator-card">{indicators?.TRI.toFixed(2)}</div>
-            <div className = "indicator-card">{indicators?.TIR.toFixed(2)}</div>
-            <div className = "indicator-card">{indicators?.LEC.toFixed(2)}</div>
-            <div className = "indicator-card">{indicators?.BPM.toFixed(2)}</div>
-            <div className = "indicator-card">{indicators?.n}</div>
-        </section>
-     );
+function IndicatorsGrid({ indicators }) {
+  return (
+    <section className="indicators-grid">
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#02C502'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Valor Actual Neto (VAN)
+                </header>
+                {indicators?.VAN.toFixed(2)}
+            </section>
+        </article>
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#06F'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Tasa Interna de Retorno (TIR)
+                </header>
+                {indicators?.TRI.toFixed(2)}
+            </section>
+        </article>
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#0FF'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Tiempo de Recuperación de Inversión (TRI)
+                </header>
+                {indicators?.TIR.toFixed(2)}
+            </section>
+        </article>
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#AD00FF'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Costo Nivelado de la Electricidad (LEC)
+                </header>
+                {indicators?.LEC.toFixed(2)}
+            </section>
+        </article>
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#FFC700'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Potencial de Biometano Generado (BPM)
+                </header>
+                {indicators?.BPM.toFixed(2)}
+            </section>
+        </article>
+      <article className="indicator-card">
+            <aside style = {{backgroundColor:'#DC0101'}}>
+
+            </aside>
+            <section>
+                <header>
+                    Eficiencia del proceso (n)
+                </header>
+                {indicators?.n}
+            </section>
+        </article>
+    </section>
+  );
 }
 
 export default IndicatorsGrid;
