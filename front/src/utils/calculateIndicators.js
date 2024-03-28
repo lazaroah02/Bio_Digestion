99e0bb3eb7a8802ab0b1beef1958c4b358f24a57
lazaroah <lazaroah02@gmail.com>
2024-03-28 15:58:57 -0400

    creating functions to calculate indicators

---
+++ front/src/utils/calculateIndicators.js
@@ -0,0 +1,19 @@
+export function calculateVAN(){
+    return 0
+}
+
+export function calculateTIR(){
+    return 0
+}
+
+export function calculateTRI(){
+    return 0
+}
+
+export function calculateLEC(){
+    return 0
+}
+
+export function calculateBPM(){
+    return 0
+}
