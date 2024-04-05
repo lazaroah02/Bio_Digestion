2c1ecc6e2c0fecf1beaec651719a33a08e9d8e27
lazaroah <lazaroah02@gmail.com>
2024-04-05 17:02:44 -0400

    adding status messages to indicators calculation

--- front/src/components/IndicatorsComponents/CalculateIndicatorsModals/ShowResult/index.jsx
+++ front/src/components/IndicatorsComponents/CalculateIndicatorsModals/ShowResult/index.jsx
@@ -1,15 +1,24 @@
 import './index.css'
+import {useToast} from '../../../../hooks/useToast'
 function ShowResult({result = null, updateIndicatorValue, indicatorName}) {
+    const {toast, showSuccessMessage, showErrorMessage} = useToast()
+    function handleSaveResult(){
+        if(result !== null){
+            updateIndicatorValue({indicatorName:indicatorName, newValue:result});
+            showSuccessMessage("Valor actualizado")
+        }else{
+            showErrorMessage("No has efectuado ningún cálculo")
+        }
+    }
     return (
         <>
+            {toast()}
             <section className = "show-result-indicator-calculation">
                 <div>Resultado:</div>
                 <span>{result?.toFixed(2)}</span>
             </section> 
             <button className = "save-result-indicator-calculation"
-            onClick={() => {
-                result !== null?updateIndicatorValue({indicatorName:indicatorName, newValue:result}):null
-            }}>Guardar Nuevo Resultado</button>
+            onClick={() => handleSaveResult()}>Guardar Nuevo Resultado</button>
         </>
     );
 }
--- front/src/components/IndicatorsComponents/CalculateIndicatorsModals/ShowVANResult/index.jsx
+++ front/src/components/IndicatorsComponents/CalculateIndicatorsModals/ShowVANResult/index.jsx
@@ -1,15 +1,26 @@
 import './index.css'
+import {useToast} from '../../../../hooks/useToast'
 
 function ShowVANResult({result = null, updateIndicatorValue}) {
+    const {toast, showSuccessMessage, showErrorMessage} = useToast()
+
+    function handleSaveResult(){
+        if(result !== null){
+            updateIndicatorValue({indicatorName:"VAN", newValue:result});
+            showSuccessMessage("Valor actualizado")
+        }else{
+            showErrorMessage("No has efectuado ningún cálculo")
+        }
+    }
+
     return ( 
         <section className = "show-VAN-result">
+            {toast()}
             <section className = "show-result-VAN-calculation">
                 <div>Resultado:</div>
                 <span>{result}</span>
             </section> 
-            <button className = "save-result-VAN-calculation" onClick={() => {
-                result !== null?updateIndicatorValue({indicatorName:"VAN", newValue:result}):null
-            }}>Guardar Nuevo Resultado</button>
+            <button className = "save-result-VAN-calculation" onClick={() => handleSaveResult()}>Guardar Nuevo Resultado</button>
         </section>
      );
 }
