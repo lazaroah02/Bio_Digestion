4982e17bda487033c3f30d480569c2b3ef6493b1
lazaroah <lazaroah02@gmail.com>
2024-04-30 11:29:00 -0400

    improving mobile mode of graphics page, showing graphics in horizontal mode in mobile

--- front/src/pages/Graphics.jsx
+++ front/src/pages/Graphics.jsx
@@ -3,9 +3,11 @@ import {useManageProjects} from '../hooks/useManageProjects'
 import Loader from '../components/Loader'
 import { AgChartsReact } from 'ag-charts-react'
 import {useEffect, useState} from 'react'
+import {useIsMobileMode} from '../hooks/useIsMobileMode'
 
 function Graphics() {
     const {projects, loadingProjects} = useManageProjects()
+    const {mobileMode} = useIsMobileMode({mobileWidth:500})
 
     const [chartsOptions, setChartsOptions] = useState({
         VAN:{
@@ -54,7 +56,7 @@ function Graphics() {
         },
     });
 
-    //update Chart on change VANpartialResults
+    //update Charts on change products
     useEffect(() => {
         let chartsOptionsCopy = {...chartsOptions}
         chartsOptionsCopy.VAN.data = projects
@@ -65,6 +67,27 @@ function Graphics() {
         chartsOptionsCopy.Z.data = projects
         setChartsOptions(chartsOptionsCopy)
     },[projects])
+
+    //show charts in horizontal when is mobile mode and vertical on Desktop
+    useEffect(() => {
+        let chartsOptionsCopy = {...chartsOptions}
+        if(mobileMode){
+            chartsOptionsCopy.VAN.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.VAN' }]
+            chartsOptionsCopy.LEC.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.LEC' }]
+            chartsOptionsCopy.TRI.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.TRI' }]
+            chartsOptionsCopy.TIR.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.TIR' }]
+            chartsOptionsCopy.BPM.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.BPM' }]
+            chartsOptionsCopy.Z.series = [{ type: 'bar', direction: 'horizontal', xKey: 'name', yKey: 'indicators.Z' }]
+        }else{
+            chartsOptionsCopy.VAN.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.VAN' }]
+            chartsOptionsCopy.LEC.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.LEC' }]
+            chartsOptionsCopy.TRI.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.TRI' }]
+            chartsOptionsCopy.TIR.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.TIR' }]
+            chartsOptionsCopy.BPM.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.BPM' }]
+            chartsOptionsCopy.Z.series = [{ type: 'bar', xKey: 'name', yKey: 'indicators.Z' }]
+        }
+        setChartsOptions(chartsOptionsCopy)
+    },[mobileMode])
 
     return ( 
         <section className = "graphics-page">
@@ -80,18 +103,23 @@ function Graphics() {
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.VAN}/>
             </div>
+            <div className = "graphics-page-graphics-separator"></div>
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.LEC} />
             </div>
+            <div className = "graphics-page-graphics-separator"></div>
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.TRI} />
             </div>
+            <div className = "graphics-page-graphics-separator"></div>
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.TIR} />
             </div>
+            <div className = "graphics-page-graphics-separator"></div>
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.BPM} />
             </div>
+            <div className = "graphics-page-graphics-separator"></div>
             <div className = "graphic-container">
                 <AgChartsReact options={chartsOptions.Z} />
             </div>
