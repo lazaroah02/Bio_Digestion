0308c062c982da2a7ee84b8a37ae2837e5c13b87
lazaroah <lazaroah02@gmail.com>
2024-04-30 13:03:38 -0400

    improving graphics page

--- front/src/pages/Graphics.jsx
+++ front/src/pages/Graphics.jsx
@@ -4,57 +4,13 @@ import Loader from '../components/Loader'
 import { AgChartsReact } from 'ag-charts-react'
 import {useEffect, useState} from 'react'
 import {useIsMobileMode} from '../hooks/useIsMobileMode'
+import {CHART_OPTIONS_INITIAL_DATA} from '../data/chartOptionsInitialData'
 
 function Graphics() {
     const {projects, loadingProjects} = useManageProjects()
     const {mobileMode} = useIsMobileMode({mobileWidth:500})
 
-    const [chartsOptions, setChartsOptions] = useState({
-        VAN:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.VAN' }],
-            title: { text: 'Valor Actual Neto (VAN)'},
-        },
-        LEC:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.LEC' }],
-            title: { text: 'Costo Nivelado de la Electricidad (LEC)' },
-        },
-        TRI:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.TRI' }],
-            subtitle: {text: "En años",},
-            title: { text: 'Tiempo de Recuperación de la Inversión (TRI)' },
-        },
-        TIR:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.TIR' }],
-            subtitle: {text: "En porciento(%)",},
-            title: { text: 'Tasa Interna de Retorno (TIR)' },
-        },
-        BPM:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.BPM' }],
-            title: { text: 'Potencial de Biometano Generado (BPM)' },
-        },
-        Z:{
-            // Data: Data to be displayed in the chart
-            data: [],
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', xKey: 'name', yKey: 'indicators.Z' }],
-            title: { text: 'Z' },
-        },
-    });
+    const [chartsOptions, setChartsOptions] = useState(CHART_OPTIONS_INITIAL_DATA);
 
     //update Charts on change products
     useEffect(() => {
