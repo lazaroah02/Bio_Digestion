b72497cebd4ed300ecc2064a9cb234978841ff52
lazaroah <lazaroah02@gmail.com>
2024-05-09 17:26:22 -0400

    adding title to design pages

--- front/src/pages/PostTreatmentBiodigestorsDesign.jsx
+++ front/src/pages/PostTreatmentBiodigestorsDesign.jsx
@@ -3,7 +3,7 @@ import './pagesStyles/design.css'
 function PostreatmentBiodigestorsDesign() {
     return ( 
         <section className = "biodigestor-design-page">
-
+            <h1 className = "biodigestor-design-title">Diseño de Biodigestores de Postratamiento</h1>
         </section> 
     );
 }
--- front/src/pages/TreatmentBiodigestorsDesign.jsx
+++ front/src/pages/TreatmentBiodigestorsDesign.jsx
@@ -3,6 +3,7 @@ import './pagesStyles/design.css'
 function TreatmentBiodigestorsDesign() {
     return ( 
         <section  className = "biodigestor-design-page">
+            <h1 className = "biodigestor-design-title">Diseño de Biodigestores de Tratamiento</h1>
             
         </section> 
     );
